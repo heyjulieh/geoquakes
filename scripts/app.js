d1ea2326a2a5046965e133aaf9690710126425f7
heyjulieh <heyjulieh@gmail.com>
2017-04-05 12:31:12 -0700

    started pseudocode and ajax

--- scripts/app.js
+++ scripts/app.js
@@ -2,7 +2,30 @@
 var weekly_quakes_endpoint = "http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_week.geojson";
 
 $(document).on("ready", function() {
+var geoquakesData = {
 
   // CODE IN HERE!
+// ajax
+$.ajax({
+  // What kind of request
+  method: "GET",
+
+  // The URL for the request
+  url: "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_week.geojson",
+
+  // The data to send aka query parameters
+  datatype: 'json',
+
+  data: $("form").serialize(),
+
+  // Code to run if the request succeeds;
+  // the response is passed to the function
+  // success: onSuccess,
+
+  // Code to run if the request fails; the raw request and
+  // status codes are passed to the function
+  // error: onError
+});
+//onsuccess function
 
 });
